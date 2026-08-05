93830ac85f3ed8255240b61701f2c9bacbf1a520
luisenriquegiraldo-hub <luisenriquegiraldo@gmail.com>
2026-08-04 21:36:30 -0500

    Replace HelloWorld with FabricaVideo composition

--- remotion/Root.tsx
+++ remotion/Root.tsx
@@ -1,28 +1,40 @@
 import { Composition } from "remotion";
-import { HelloWorld, helloWorldCompSchema } from "./HelloWorld";
-
-// Each <Composition> is an entry in the sidebar!
+import { FabricaVideo, fabricaVideoSchema, FPS } from "./FabricaVideo";
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
       <Composition
-        // You can take the "id" to render a video:
-        // npx remotion render HelloWorld
-        id="HelloWorld"
-        component={HelloWorld}
-        durationInFrames={800}
-        fps={30}
-        width={1920}
-        height={1080}
-        // You can override these props for each render:
-        // https://www.remotion.dev/docs/parametrized-rendering
-        schema={helloWorldCompSchema}
+        id="FabricaVideo"
+        component={FabricaVideo}
+        fps={FPS}
+        width={1080}
+        height={1920}
+        // durationInFrames es solo el valor por defecto en el Studio;
+        // calculateMetadata lo recalcula en cada render segun la suma de escenas.
+        durationInFrames={150}
+        schema={fabricaVideoSchema}
         defaultProps={{
-          titleText: "Render Server Template",
-          titleColor: "#000000",
-          logoColor1: "#91EAE4",
-          logoColor2: "#86A8E7",
+          scenes: [
+            {
+              imageUrl:
+                "https://placehold.co/1080x1920/0b1120/white?text=Fabrica+de+Contenido",
+              durationSeg: 5,
+            },
+          ],
+          voiceUrl: "",
+          musicUrl: "",
+          captions: [],
+        }}
+        calculateMetadata={async ({ props }) => {
+          const totalSeg = props.scenes.reduce(
+            (sum, scene) => sum + scene.durationSeg,
+            0,
+          );
+
+          return {
+            durationInFrames: Math.max(1, Math.round(totalSeg * FPS)),
+          };
         }}
       />
     </>
